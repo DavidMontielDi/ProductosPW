aa770b795ba42bec64b9a49626e8bdf1ab617194
DavidMontielDi <65210412+DavidMontielDi@users.noreply.github.com>
2020-05-12 13:46:29 -0700

    Encabezado modificado

--- src/Inicio/Encabezado.js
+++ src/Inicio/Encabezado.js
@@ -10,7 +10,7 @@ export default function Encabezado() {
         <div class="row h-100 align-items-center">
           <div class="col-lg-12">
             <h1 class="display-4 text-white mt-5 mb-2">
-              Devz Store - David Montiel 
+              Devz Store - David Montiel
             </h1>
             <p class="lead mb-5 text-white-50">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non
