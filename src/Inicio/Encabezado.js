f550eb9913065eedc4db2b462f5b57dd7c3be83c
DavidMontielDi <65210412+DavidMontielDi@users.noreply.github.com>
2020-05-12 13:05:52 -0700

    Modificacion de fondo

--- src/Inicio/Encabezado.js
+++ src/Inicio/Encabezado.js
@@ -6,10 +6,10 @@ const colorFondo = {
 export default function Encabezado() {
   return (
     <header class="bg-primary py-5 mb-5">
-      <div class="container h-100">
+      <div class="container h-100" style={colorFondo}>
         <div class="row h-100 align-items-center">
           <div class="col-lg-12">
-            <h1 style={colorFondo} class="display-4 text-white mt-5 mb-2">
+            <h1 class="display-4 text-white mt-5 mb-2">
               Devz Store - David Montiel 
             </h1>
             <p class="lead mb-5 text-white-50">
