e5be1ee1864ea0121c740046ec1bf5cac1186626
DavidMontielDi <65210412+DavidMontielDi@users.noreply.github.com>
2020-05-12 13:59:38 -0700

    Modificacion color

--- src/Inicio/Encabezado.js
+++ src/Inicio/Encabezado.js
@@ -5,8 +5,8 @@ const colorFondo = {
 
 export default function Encabezado() {
   return (
-    <header class="bg-primary py-5 mb-5">
-      <div class="container h-100" style={colorFondo}>
+    <header class="bg-primary py-5 mr-5" style={colorFondo}>
+      <div class="container h-100" >
         <div class="row h-100 align-items-center">
           <div class="col-lg-12">
             <h1 class="display-4 text-white mt-5 mb-2">
